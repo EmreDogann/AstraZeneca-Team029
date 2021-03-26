95936b899e0106eeacb18d46009e6d3ad7118043
Aum <imaum2208@gmail.com>
2021-03-26 20:39:32 +0000

    drag drop and left button

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
 from DirectoryManager import DirectoryManager
+from FileSpellChecker import FileSpellChecker
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.core.window import Window
+import platform
 Window.clearcolor = (1, 1, 1, 1)
+
+# path =r"C:\Users\Emre\Downloads\GitHub\testDir\test.txt" 
 
 class HomeScreen(Widget):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.path = ''
+
 
     def fileImport(self, operationType):
-        self.directoryManager = DirectoryManager(r"C:\Users\Emre\Downloads\GitHub\testDir\test.txt")
+        self.directoryManager = DirectoryManager(self.path)
         if (operationType == "Directory"):
             self.directoryManager.getDirContents()
+            self.filesDropped()
         elif (operationType == "Deep"):
             self.words = self.directoryManager.getFileContents()
+
     
     def fileRename(self, operationType):
-        if (operationType == "DirectoryRename"):
+        if (operationType == "Directory"):
             self.directoryManager.renameDir(self.directoryManager.dirContents[0], "test2.txt")
         elif (operationType == "Deep"):
             self.directoryManager.createNewFile(self.words)
+
+    def onFileFolderButtonPressed(self):
+        self.fileSpellChecker = FileSpellChecker(self.directoryManager.dirContents)
+        print(self.fileSpellChecker.spellCheck()) 
+        
 
 class FileNameScreen(Widget):
     def __init__(self, **kwargs):
@@ -29,12 +42,25 @@ class FileNameScreen(Widget):
         print("hi")
 
 class MainApp(App):
+
     def build(self):
-        homeScreen = HomeScreen()
-        # mainScreen.fileImport("Deep")
+        # binding the drop file 
+        Window.bind(on_dropfile=self._on_file_drop)
+        
+        self.homeScreen = HomeScreen()
+
+        # self.homeScreen.fileImport("Directory")
         # mainScreen.fileRename("Deep")
         # mainScreen.changeFileContents("Deep")
-        return homeScreen
+        return self.homeScreen
+
+    def _on_file_drop(self, window, file_path):
+        print(file_path)
+        if(platform.system() == "Darwin"):
+            self.homeScreen.path = file_path.decode("utf-8")
+        elif(platform.system() == "Windows"):
+           self.homeScreen.path = file_path.decode("utf-16") 
+        return
 
 
 if __name__ == '__main__':
