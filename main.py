731e028d9d911b0e1c3618d0e34878c5287c5edd
Emre Dogan <emre.dogan.1@protonmail.com>
2021-03-26 19:43:20 +0000

    Kivy Main Screen Done

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from DirectoryManager import DirectoryManager
 from kivy.app import App
 from kivy.uix.widget import Widget
-# from kivy.properties import ObjectProperty
-# from kivy.graphics.texture import Texture
 from kivy.core.window import Window
-# Window.clearcolor = (1, 1, 1, 1)
+from kivy.graphics.svg import Svg
+Window.clearcolor = (1, 1, 1, 1)
 
 class HomeScreen(Widget):
     def __init__(self, **kwargs):
